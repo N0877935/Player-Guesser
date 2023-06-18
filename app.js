5b1c6b931e591099ce395df45873c0f3f306db26
Joe Finlay <joe.finlay@icloud.com>
2023-06-18 21:46:35 +0100

    promise returns value which is passed into object as EJS variable to appear when page is loaded

--- app.js
+++ app.js
@@ -1,57 +1,52 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const https = require('https');
-const wiki = require('wikijs').default;
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const wiki = require("wikijs").default;
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+const firstInfo = [];
 
-app.get('/', (req, res) => {
-  
-  res.render('home');
+app.get("/", (req, res) => {
+  const day = date.getDate();
 
-  const key = 'bn0t5U326A3ITj1QxP1ZHYyHeELIEEu91Q8Tjfhq3C7GvDFe4eMMATztxH40#';
+  const key = "bn0t5U326A3ITj1QxP1ZHYyHeELIEEu91Q8Tjfhq3C7GvDFe4eMMATztxH40#";
   const url = `https://soccer.sportmonks.com/api/v2.0/players/96611?api_token=${key}`;
 
   https.get(url, (response) => {
     const data = [];
-    response.on('data', (d) => {
-      data.push(d);      
-     }).on('end', () => {
-      const buffer = Buffer.concat(data);
-      const obj = JSON.parse(buffer.toString());
-      const randomPlayer = obj.data.display_name;
-      showPlayerInfo(randomPlayer);
-      // res.render('/', )
-     })
+    response
+      .on("data", (d) => {
+        data.push(d);
+      })
+      .on("end", () => {
+        const buffer = Buffer.concat(data);
+        const obj = JSON.parse(buffer.toString());
+        const randomPlayer = obj.data.display_name;
+        wiki()
+          .page(randomPlayer)
+          .then((page) => page.info("years1"))
+          .then((response) => {
+            res.render("home", { yearsOne: response, todayDate: day });
+          });
+      });
   });
+});
 
-})
-
-app.post('/', (req, res) => {
-  res.status(204).send()
-
-})
-
+app.post("/", (req, res) => {
+  res.status(204).send();
+});
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
-})
-
-
-function showPlayerInfo(randPlayer) {
-  const player = wiki().page(randPlayer);
-
-      player.then(page => page.info()).then(console.log);
-}
-
+});
 
 
